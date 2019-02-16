5a68daf1450ee2655a022344e7b6f9e4132dff4a
Hugo Zapata <hugozap@gmail.com>
2019-02-16 01:41:15 -0500

    grid-base-2

--- grid-base-2/shader.fs
+++ grid-base-2/shader.fs
@@ -1,23 +1,36 @@
 uniform vec2 u_resolution;
 uniform float u_time;
 
-/* Usar rutina que usa sin para pintar lineas */
 
-float lines(vec2 st, float thick) {
-	float val = sin(st.x*100.);
-	// if val is 1 draw line
-	return smoothstep(1. - thick, 1., val);
+vec2 tile(vec2 st, float size) {
+	st = st * size;
+	st = fract(st);
+	return st;
 }
 
-float grid(vec2 st, float thick) {
-	float lh = lines(st, thick);
-	float lv = lines(st * rotate2d(PI/2.), thick);
-	return lv + lh;
+float grid(vec2 uv) {
+	float l = max(smoothstep(0., 0.1, uv.x) +
+		smoothstep(0., 0.1, uv.y), 1.);
+	return l;
+}
+
+float box(vec2 _st, vec2 _size, float _smoothEdges){
+    _size = vec2(0.5)-_size*0.5;
+    vec2 aa = vec2(_smoothEdges*0.5);
+    vec2 uv = smoothstep(_size,_size+aa,_st);
+    uv *= smoothstep(_size,_size+aa,vec2(1.0)-_st);
+    return uv.x*uv.y;
 }
 
 void main() {
 	vec2 uv= gl_FragCoord.xy / u_resolution.xy;
-	float l = grid(uv, 0.05);
-	vec3 finalColor = vec3(1.) * (1. - l);
+	//dividir espacio en 10
+	float d = distance(uv, vec2(0.5, 0.5));
+	uv += u_time/10.;
+	//entre más cerca del centro más fuerte la distorsión
+	uv = tile(uv + snoise(uv)*d, 10.);
+	float l = max(smoothstep(0., 0.1, uv.x) +
+		smoothstep(0., 0.1, uv.y), 1.);
+	vec3 finalColor = vec3(1., 0.5, 0.1) * l;
 	gl_FragColor = vec4(finalColor, 1.0);
 }
